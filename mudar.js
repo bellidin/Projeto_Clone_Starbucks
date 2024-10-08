fb123034695e2540e0805bddaa44f389e566b024
bellidin <nardineduc@gmail.com>
2024-10-08 08:30:35 -0300

    Projeto Clone Starbucks Finalização

--- mudar.js
+++ mudar.js
@@ -1,10 +1,10 @@
 // primeira parte Habilitado/Desabilitado
-function ToggleMenu(){
+function toggleMenu(){
     var menuToggle = document.querySelector('.toggle');
     var links = document.querySelector('.links');
-    // troca o estado da classe toggle para ativo
+    // troca o estado da classe .toggle para ativo
     menuToggle.classList.toggle('active')
-    // troca o estado da classe .links para ativo
+    // troca o estado da classe .links para ativo 
     links.classList.toggle('active')
 }
 
